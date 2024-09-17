1a9b874c202a127aa1310327c0fbe87a83df24c0
Lola <mdgr0010@red.ujaen.es>
2024-09-17 21:08:02 +0200

    Inicialización GLFW

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+//IMPORTANTE: El include de GLAD debe estar siempre ANTES de el de GLFW
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Starting application PAG - Prueba 01" << std::endl;
+
+    //Inicializa GLFW. Es un proceso que sólo debe realizarse una vez en la aplicación
+    if (glfwInit() != GLFW_TRUE) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
     return 0;
 }
