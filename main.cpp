076794a5f0a8035784a11b6af6cd46984151b231
Lola <mdgr0010@red.ujaen.es>
2024-09-17 21:15:56 +0200

    Definición OpenGL Core 4.1.

--- main.cpp
+++ main.cpp
@@ -11,5 +11,14 @@ int main() {
         std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
+
+    //Definimos las características que queremos que tenga el contexto gráfico
+    //OpenGL de la ventana que vamos a crear. Por ejemplo, el número de muestras o el
+    //modo Core Profile.
+    glfwWindowHint(GLFW_SAMPLES, 4); //Activa antialiasing con 4 muestras.
+    //Las siguientes ordenes activan un contexto OpenGL Core Profile 4.1.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     return 0;
 }
