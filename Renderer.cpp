8b5f73d0563378a1231023141470904c8eb0dc71
Lola <mdgr0010@red.ujaen.es>
2024-09-20 18:45:48 +0200

    Creación clase Renderer

---
+++ Renderer.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lola on 20/09/2024.
+//
+
+#include "Renderer.h"
+
+namespace PAG {
+} // PAG
---
+++ Renderer.h
@@ -0,0 +1,16 @@
+//
+// Created by Lola on 20/09/2024.
+//
+
+#ifndef RENDERER_H
+#define RENDERER_H
+
+namespace PAG {
+
+class Renderer {
+
+};
+
+} // PAG
+
+#endif //RENDERER_H
