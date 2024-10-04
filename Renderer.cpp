23efcbe14c3052e16d894c3e82c02eeeea79fa64
Lola <mdgr0010@red.ujaen.es>
2024-10-04 02:38:08 +0200

    actualizacion refresco

--- Renderer.cpp
+++ Renderer.cpp
@@ -40,6 +40,11 @@ namespace PAG {
     //Método para hacer el refresco de la escena
     void Renderer::refrescar() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glUseProgram(idSP);
+        glBindVertexArray(idVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, idIBO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
     }
 
     //Método para reedimensinar la escena
