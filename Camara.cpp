7c57e864fa03ee6649572e2a2ad0df5ccdff5cd6
Lola <mdgr0010@red.ujaen.es>
2024-10-25 03:39:48 +0200

    Creación clase Cámara

--- Camara.cpp
+++ Camara.cpp
@@ -1,8 +1,67 @@
-//
-// Created by Lola on 25/10/2024.
-//
-
+#include <glad/glad.h>
+#include <glm/glm.hpp>
 #include "Camara.h"
 
 namespace PAG {
+    Camara::Camara() {
+
+    }
+
+    Camara::~Camara() {
+
+    }
+
+    void Camara::init() {}
+
+    void Camara::update() {}
+
+    void Camara::render() {}
+
+    GLfloat Camara::radianes(int anguloSexagesimal) {
+        GLfloat anguloRadianes = glm::radians(anguloSexagesimal);
+        return anguloRadianes;
+    }
+
+    glm::mat4 Camara::transVision(int pos, int lookAt, int up) {
+        glm::mat4 matriz = glm::lookAt(pos, lookAt, up);
+        return matriz;
+    }
+
+    glm::mat4 Camara::transProyeccion(int fovY, int aspecto, int zNear, int zFar) {
+        glm::mat4 matriz = glm::perspective(fovY, aspecto, zNear, zFar);
+        return matriz;
+    }
+
+    glm::mat4 Camara::translacion(int posicion, int otraPosicion) {
+        glm::mat4 m1 = glm::translate(posicion); //Devuelve una matriz con solo la traslacion
+
+        glm::mat4 m2 = glm::translate(m1, otraPosicion); //Calcula la traslacion, multiplica m1*traslacion y devuelve el resultado
+
+        return m2;
+    }
+
+    glm::mat4 Camara::rotacion(int angulo, glm::vec3 eje) {
+        glm::mat4 m1 = glm::rotate(angulo, eje); //Devuelve una matriz con solo la rotacion
+
+        glm::mat4 m2 = glm::rotate(m1, angulo, eje); // Calcula la rotación, multiplica m1*rotacion y devuelve el resultado. m1 no cambia
+
+        return m2;
+    }
+
+    glm::vec3 Camara::normaliza(glm::vec3 vector) {
+        glm::vec3 normalizado = glm::normalize(vector);
+        return normalizado;
+    }
+
+    glm::vec3 Camara::cruce(glm::vec3 vector1, glm::vec3 vector2) {
+        glm::vec3 v3 = glm::cross(vector1, vector2);
+        return normalize(v3);
+    }
+
+    glm::bvec3 Camara::comparar(glm::vec3 vector1, glm::vec3 vector2) {
+        glm::bvec3 v3 = equal(vector1, vector2, glm::epsilon<float>());
+        return v3;
+    }
+
+
 } // PAG
--- Camara.h
+++ Camara.h
@@ -7,9 +7,29 @@
 
 namespace PAG {
 
-class Camara {
 
-};
+    class Camara {
+        private:
+            float pan;
+            float tilt;
+            float dolly;
+            float orbit;
+            float zoom;
+        public:
+            Camara();
+            ~Camara();
+            void init();
+            void update();
+            void render();
+            GLfloat radianes(int anguloSexagesimal);
+            glm::mat4 transVision(int pos, int lookAt, int up);
+            glm::mat4 transProyeccion(int fovY, int aspecto, int zNear, int zFar);
+            glm::mat4 translacion(int posicion, int otraPosicion);
+            glm::mat4 rotacion(int angulo, glm::vec3 eje);
+            glm::vec3 normaliza(glm::vec3 vector);
+            glm::vec3 cruce(glm::vec3 vector1, glm::vec3 vector2);
+            glm::bvec3 comparar(glm::vec3 vector1, glm::vec3 vector2);
+    };
 
 } // PAG
 
