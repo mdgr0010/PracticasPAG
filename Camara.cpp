5a9b2e2559537462e5ea1e63a7901956b8208ac6
Lola <mdgr0010@red.ujaen.es>
2024-10-25 03:08:27 +0200

    Creación clase Cámara

---
+++ Camara.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lola on 25/10/2024.
+//
+
+#include "Camara.h"
+
+namespace PAG {
+} // PAG
---
+++ Camara.h
@@ -0,0 +1,16 @@
+//
+// Created by Lola on 25/10/2024.
+//
+
+#ifndef CAMARA_H
+#define CAMARA_H
+
+namespace PAG {
+
+class Camara {
+
+};
+
+} // PAG
+
+#endif //CAMARA_H
